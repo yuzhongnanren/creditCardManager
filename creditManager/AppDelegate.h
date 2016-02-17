e655e19817b99d92ce9859e51c697adbb800675a
周勇 <zhouyong@haodai.com>
2016-02-17 16:15:06 +0800

    Initial Commit

---
+++ creditManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  creditManager
+//
+//  Created by haodai on 16/2/17.
+//  Copyright © 2016年 haodai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
